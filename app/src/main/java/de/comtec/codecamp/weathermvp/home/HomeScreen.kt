d7330481e7f8324a1ce950aa6ce7d5bdbcb827f4
Lars Mathuseck <lars.mathuseck@comtec.eecs.uni-kassel.de>
2023-11-10 17:42:59 +0100

    UI Improvements

--- app/src/main/java/de/comtec/codecamp/weathermvp/home/HomeScreen.kt
+++ app/src/main/java/de/comtec/codecamp/weathermvp/home/HomeScreen.kt
@@ -1,73 +1,139 @@
 package de.comtec.codecamp.weathermvp.home
 
-import android.health.connect.datatypes.units.Temperature
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import de.comtec.codecamp.weathermvp.R
+import eu.bambooapps.material3.pullrefresh.PullRefreshIndicator
+import eu.bambooapps.material3.pullrefresh.pullRefresh
+import eu.bambooapps.material3.pullrefresh.rememberPullRefreshState
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
 
     val weather = viewModel.weatherData.value
     val loading = viewModel.loading.value
-    val network = viewModel.internetAccess.collectAsState().value
+    val hasNetwork = viewModel.internetAccess.collectAsState().value
 
-    // TODO: Do this only, if network is available
-    LaunchedEffect(key1 = Unit, block = {
+    val pullRefreshState = rememberPullRefreshState(refreshing = loading, onRefresh = {
         viewModel.fetchWeatherData()
     })
 
-    // TODO handle no internet access on the UI
+    LaunchedEffect(key1 = hasNetwork, block = {
+        if (hasNetwork) {
+            viewModel.fetchWeatherData()
+        }
+    })
 
-    Column {
-
-        if (loading) {
-            Column {
-                Text(text = "Loading...")
-                Spacer(modifier = Modifier.size(16.dp))
-                CircularProgressIndicator()
+    Box {
+        LazyColumn(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+                .pullRefresh(pullRefreshState),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            if (!hasNetwork) {
+                item {
+                    NoNetwork()
+                }
             }
-        } else {
-            TemperatureInfo(weather?.current?.temperature ?: 20.0)
-            PrecipitationInfo(weather?.current?.precipitationProbability ?: 0.0)
-            Button(onClick = {
-                viewModel.fetchWeatherData()
-            }) {
-                Text(text = "Refresh")
+            item {
+                TemperatureInfo(weather?.current?.temperature)
+            }
+            item {
+                PrecipitationInfo(weather?.current?.precipitationProbability)
+            }
+            // TODO add a composable to show last update time
+        }
+        PullRefreshIndicator(
+            refreshing = loading,
+            state = pullRefreshState,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun NoNetwork() {
+    Card {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                // Error icon
+                Icon(painterResource(id = R.drawable.ic_error), contentDescription = null)
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(text = "No internet connection", fontSize = 16.sp)
             }
         }
-
     }
 }
 
 @Preview
 @Composable
-fun TemperatureInfo(temperature: Double = 18.0) {
-    Card(modifier = Modifier.padding(32.dp)) {
-        Text(text = "$temperature °C")
+fun TemperatureInfo(temperature: Double? = null) {
+    Card {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "${temperature ?: "-"}°C", fontSize = 28.sp)
+            Text(text = "Temperature", fontSize = 12.sp)
+        }
     }
 }
 
 @Preview
 @Composable
-fun PrecipitationInfo(precipitation: Double = 0.0) {
-    Card(modifier = Modifier.padding(32.dp)) {
-        Text(text = "$precipitation %")
+fun PrecipitationInfo(precipitation: Double? = null) {
+    Card {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "${precipitation ?: "-"}%", fontSize = 28.sp)
+            Text(text = "Precipitation", fontSize = 12.sp)
+        }
     }
 }
 
